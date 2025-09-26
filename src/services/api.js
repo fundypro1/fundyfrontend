803ea368b0f6825455aed059507d54f2694b55da
fundypro1 <fundypro1@gmail.com>
2025-09-26 08:34:56 -0400

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'https://fundybackend-9.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const apiClient = axios.create({
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 // src/services/AuthService.js
 import axios from "axios";
 
-const API_URL = "https://fundybackend-9.onrender.com"; // ⚠️ change for production
+const API_URL = import.meta.env.VITE_API_URL;
 
 // ============ AXIOS INSTANCE ============
 const apiClient = axios.create({
